f6ccfa5774ad895239d908a2993e9683e74aa766
Jonathan Castañeda <jscastaneda@modusoftware.com>
2022-11-03 21:31:21 -0500

    Cambio de ORM Diesel a SQLx para conexión a base de datos

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,39 @@
 #[macro_use]
 extern crate rocket;
-#[macro_use]
-extern crate diesel;
 
 mod models;
-mod schema;
 
 use dotenvy::dotenv;
 use models::{NewPostHandler, Post};
 use rocket::{http::Status, serde::json::Json};
+use rocket_db_pools::sqlx;
+use rocket_db_pools::Database;
 use rocket_dyn_templates::{context, Template};
-use rocket_sync_db_pools::database;
 
+#[derive(Database)]
 #[database("pg_db")]
-struct Db(rocket_sync_db_pools::diesel::PgConnection);
+pub struct Db(sqlx::PgPool);
 
 #[get("/")]
-async fn index(db: Db) -> Result<Template, Status> {
-    match db.run(|conn| Post::find_all(conn)).await {
+async fn index(db: &Db) -> Result<Template, Status> {
+    match Post::find_all(&db.0).await {
         Ok(posts) => Ok(Template::render("index", context! { posts: posts })),
         Err(_) => Err(Status::InternalServerError),
     }
 }
 
 #[get("/blog/<blog_slug>")]
-async fn get_post(db: Db, blog_slug: String) -> Result<Template, Status> {
-    match db
-        .run(move |conn| Post::find_by_slug(conn, &blog_slug))
-        .await
-    {
+async fn get_post(db: &Db, blog_slug: String) -> Result<Template, Status> {
+    match Post::find_by_slug(&db.0, &blog_slug).await {
         Ok(post) => Ok(Template::render("post", context! { post: post })),
-        Err(err) => {
-            if diesel::result::Error::NotFound == err {
-                return Err(Status::NotFound);
-            }
-
-            Err(Status::InternalServerError)
-        }
+        Err(sqlx::error::Error::RowNotFound) => Err(Status::NotFound),
+        Err(_) => Err(Status::InternalServerError),
     }
 }
 
 #[post("/new_post", data = "<item>")]
-async fn new_post(db: Db, item: Json<NewPostHandler>) -> Result<Json<Post>, Status> {
-    match db.run(move |conn| Post::create_post(conn, &item)).await {
+async fn new_post(db: &Db, item: Json<NewPostHandler>) -> Result<Json<Post>, Status> {
+    match Post::create_post(&db.0, &item).await {
         Ok(post) => Ok(Json(post)),
         Err(_) => Err(Status::InternalServerError),
     }
@@ -55,5 +46,5 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, get_post, new_post])
         .attach(Template::fairing())
-        .attach(Db::fairing())
+        .attach(Db::init())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
-use diesel::prelude::*;
 use rocket::serde::{Deserialize, Serialize};
+use sqlx::{error::Error, PgPool, Row};
 
-#[derive(Queryable, Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Post {
     pub id: i32,
@@ -12,37 +12,63 @@ pub struct Post {
 
 impl Post {
     fn slugify(title: &String) -> String {
-        title.replace(" ", "to").to_lowercase()
+        title.replace(" ", "").to_lowercase()
     }
 
-    pub fn find_all(conn: &mut PgConnection) -> Result<Vec<Post>, diesel::result::Error> {
-        posts::table.load::<Post>(conn)
+    pub async fn find_all(pool: &PgPool) -> Result<Vec<Post>, Error> {
+        let rows = sqlx::query("SELECT * FROM posts").fetch_all(pool).await?;
+
+        let result = rows
+            .iter()
+            .map(|row| Post {
+                id: row.get::<i32, _>("id"),
+                title: row.get::<String, _>("title"),
+                slug: row.get::<String, _>("slug"),
+                body: row.get::<String, _>("body"),
+            })
+            .collect::<Vec<Post>>();
+
+        Ok(result)
     }
 
-    pub fn find_by_slug(
-        conn: &mut PgConnection,
-        slug: &str,
-    ) -> Result<Post, diesel::result::Error> {
-        posts::table
-            .filter(posts::slug.eq(slug))
-            .first::<Post>(conn)
+    pub async fn find_by_slug(pool: &PgPool, slug: &str) -> Result<Post, Error> {
+        let row = sqlx::query("SELECT * FROM posts WHERE slug = $1")
+            .bind(slug)
+            .fetch_one(pool)
+            .await?;
+
+        Ok(Post {
+            id: row.get::<i32, _>("id"),
+            title: row.get::<String, _>("title"),
+            slug: row.get::<String, _>("slug"),
+            body: row.get::<String, _>("body"),
+        })
     }
 
-    pub fn create_post<'a>(
-        conn: &mut PgConnection,
-        post: &NewPostHandler,
-    ) -> Result<Post, diesel::result::Error> {
+    pub async fn create_post<'a>(pool: &PgPool, post: &NewPostHandler) -> Result<Post, Error> {
         let slug = Post::slugify(&post.title);
 
-        let new_post = NewPost {
-            title: &post.title,
-            body: &post.body,
-            slug: &slug,
-        };
+        let row = sqlx::query(
+            "
+INSERT INTO posts (title, slug, body)
+VALUES ($1, $2, $3)
+RETURNING id
+        ",
+        )
+        .bind(&post.title)
+        .bind(&slug)
+        .bind(&post.body)
+        .fetch_one(pool)
+        .await?;
 
-        diesel::insert_into(posts::table)
-            .values(new_post)
-            .get_result::<Post>(conn)
+        let id = row.get::<i32, _>("id");
+
+        Ok(Post {
+            id: id,
+            title: post.title.clone(),
+            slug: slug,
+            body: post.body.clone(),
+        })
     }
 }
 
@@ -51,15 +77,4 @@ impl Post {
 pub struct NewPostHandler {
     pub title: String,
     pub body: String,
-}
-
-use super::schema::posts;
-
-#[derive(Insertable, Deserialize, Serialize)]
-#[serde(crate = "rocket::serde")]
-#[table_name = "posts"]
-pub struct NewPost<'a> {
-    pub title: &'a str,
-    pub body: &'a str,
-    pub slug: &'a str,
 }
